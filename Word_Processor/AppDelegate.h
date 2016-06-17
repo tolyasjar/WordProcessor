947dcff361cade1e8c9704b517e1c75890221f7b
tolyasjar <toleenj@gmail.com>
2016-06-17 13:03:46 -0500

    Initial Commit

---
+++ Word_Processor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Word_Processor
+//
+//  Created by Toleen Jaradat on 6/17/16.
+//  Copyright © 2016 Toleen Jaradat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Word_Processor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Word_Processor
+//
+//  Created by Toleen Jaradat on 6/17/16.
+//  Copyright © 2016 Toleen Jaradat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
